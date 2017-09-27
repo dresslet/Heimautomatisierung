891fe45b40facdfa860aec688f58d914d7f5dca3
dressler <thomas.dressler@piobyte.de>
2017-09-27 12:04:38 +0200

    -

--- app/src/main/java/de/piobyte/dressler/heimautomatisierung/activity/LoginActivity.java
+++ app/src/main/java/de/piobyte/dressler/heimautomatisierung/activity/LoginActivity.java
@@ -1,10 +1,10 @@
 package de.piobyte.dressler.heimautomatisierung.activity;
 
-import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Color;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,15 +12,14 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import de.piobyte.dressler.heimautomatisierung.R;
-
-/**
- * Created by Piobyte-Gast on 20.09.2017.
- */
+import de.piobyte.dressler.heimautomatisierung.rest.HttpHandler;
 
 public class LoginActivity extends AppCompatActivity{
     Button loginButton;
     EditText ipAdressText, passwordText;
     TextView loginText;
+    private Boolean apiConnStat = false;
+    private String password, ip;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,26 +27,55 @@ public class LoginActivity extends AppCompatActivity{
         setContentView(R.layout.activity_login);
         //getSupportActionBar().show();
 
-        loginButton = (Button)findViewById(R.id.login_button);
-        ipAdressText = (EditText)findViewById(R.id.ip_adress);
-        passwordText = (EditText)findViewById(R.id.password);
-        loginText = (TextView)findViewById(R.id.login_text);
+        loginButton = (Button) findViewById(R.id.login_button);
+        ipAdressText = (EditText) findViewById(R.id.ip_adress);
+        passwordText = (EditText) findViewById(R.id.password);
+        loginText = (TextView) findViewById(R.id.login_text);
 
         loginText.setText("Bitte geben Sie ihre Anmeldedaten ein.");
-
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(ipAdressText.getText().toString().equals("1.") &&
-                        passwordText.getText().toString().equals("admin")) {
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-                    startActivity(intent);
-                }else{
-                    Toast.makeText(getApplicationContext(), "Wrong Credentials",Toast.LENGTH_SHORT).show();
-                }
+                ip = "http://" + ipAdressText.getText().toString() + ":8123/api/";
+                password = passwordText.getText().toString();
+                new ApiLogin().execute();
             }
         });
+
+
+    }
+    private class ApiLogin extends AsyncTask<Void, Void, Void> {
+        @Override
+            protected void onPreExecute() {
+                super.onPreExecute();
+                Toast.makeText(LoginActivity.this,"Verbindungstest zu HomeAssistant...",Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            protected Void doInBackground(Void... arg0) {
+                HttpHandler tryApiConn = new HttpHandler();
+                String apiTestStr = tryApiConn.makeServiceCall(ip, password);
+                Log.d("JSONSTRING", "" + apiTestStr);
+                if(apiTestStr != null)
+                    apiConnStat = tryApiConn.lookForApiConn(apiTestStr);
+                else
+                    apiConnStat = false;
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void result) {
+                super.onPostExecute(result);
+                if (!apiConnStat) {
+                    Toast.makeText(getApplicationContext(), "Falsche Logindaten oder keine Netzwerkverbindung", Toast.LENGTH_LONG).show();
+                    //apiConnStat = false;
+                }else{
+                    Toast.makeText(getApplicationContext(), "Verbindung erfolgreich! - Sie sind eingeloggt.", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                }
+            }
     }
 }
 
--- app/src/main/java/de/piobyte/dressler/heimautomatisierung/rest/HttpHandler.java
+++ app/src/main/java/de/piobyte/dressler/heimautomatisierung/rest/HttpHandler.java
@@ -1,6 +1,11 @@
 package de.piobyte.dressler.heimautomatisierung.rest;
 
 import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedReader;
@@ -11,22 +16,31 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.ProtocolException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import de.piobyte.dressler.heimautomatisierung.model.hADevice;
 
 public class HttpHandler {
+    private String HOMEASSISTANTURL = "http://10.2.10.102:8123/";
 
 
     public HttpHandler() {
     }
 
-    public String makeServiceCall(String reqUrl) {
+    public String makeServiceCall(String reqUrl, String password) {
         String response = null;
         try {
-            URL url = new URL(reqUrl);
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestMethod("GET");
-            // read the response
-            InputStream in = new BufferedInputStream(conn.getInputStream());
-            response = convertStreamToString(in);
+            URL url = new URL(reqUrl); //"http://swapi.co/api/people/?page=");  //HOMEASSISTANTURL + reqUrl + "?api_password=PASSWORD");
+            HttpURLConnection httpclient = (HttpURLConnection) url.openConnection();
+            httpclient.setRequestProperty("x-ha-access", password);
+            httpclient.setRequestMethod("GET");
+            httpclient.setRequestProperty("Content-Type", "application/json");
+
+            InputStream in = new BufferedInputStream(httpclient.getInputStream());
+            response = convertStreamToJson(in);
+
         } catch (MalformedURLException e) {
             Log.e("REST", "MalformedURLException: " + e.getMessage());
         } catch (ProtocolException e) {
@@ -39,10 +53,9 @@ public class HttpHandler {
         return response;
     }
 
-    private String convertStreamToString(InputStream is) {
+    private String convertStreamToJson(InputStream is) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         StringBuilder sb = new StringBuilder();
-
         String line;
         try {
             while ((line = reader.readLine()) != null) {
@@ -57,7 +70,53 @@ public class HttpHandler {
                 e.printStackTrace();
             }
         }
-
         return sb.toString();
+    }
+
+    public Boolean lookForApiConn(String jsonStr){
+        Boolean erg = false;
+        ArrayList<hADevice> deviceList = new ArrayList<>();
+        try {
+            JSONObject jsonObj = new JSONObject(jsonStr);
+
+            String apiConn = jsonObj.getString("message");
+            if(apiConn.equals("API running.")){
+                erg = true;
+            }
+        } catch (final JSONException e) {
+            Log.e("JSON", "Json parsing error: " + e.getMessage());
+        }
+        return erg;
+    }
+
+    public ArrayList<hADevice> getDevicesfromJson(String jsonStr){
+
+        ArrayList<hADevice> deviceList = new ArrayList<>();
+            try {
+                JSONObject jsonObj = new JSONObject(jsonStr);
+
+                JSONArray devices = jsonObj.getJSONArray("results");
+
+                // looping through All Contacts
+                for (int i = 0; i < devices.length(); i++) {
+                    hADevice newDevice = new hADevice();
+                    JSONObject c = devices.getJSONObject(i);
+
+                    String name = c.getString("name");
+                    String email = c.getString("homeworld");
+                    String gender = c.getString("gender");
+
+                    HttpHandler test = new HttpHandler();
+                    String ur2l = c.getString("homeworld");
+                    String jsonStrs = test.makeServiceCall("test", "test");
+                    JSONObject jsonObj2 = new JSONObject(jsonStrs);
+                    String namehw = jsonObj2.getString("name");
+
+                    deviceList.add(newDevice);
+                }
+            } catch (final JSONException e) {
+                Log.e("JSON", "Json parsing error: " + e.getMessage());
+            }
+        return deviceList;
     }
 }
